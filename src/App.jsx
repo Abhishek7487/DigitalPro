36ec71e3b3ce856705826ec098b192a69e35a2b7
Abhishek7487 <dellabhishek7487@gmail.com>
2024-05-19 19:01:03 +0530

    Setting routes using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+
 function App() {
   return (
-    <>
-      <div>
-        <p>Hello DigitalPro!</p>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
