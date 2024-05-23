3788e4d292de41b3ee0e4f8e9d3bce0c9d93add2
Abhishek7487 <dellabhishek7487@gmail.com>
2024-05-23 20:28:41 +0530

    Created a Styled Header

--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -12,7 +12,45 @@ export const StyledPageNotFound = styled.main`
   align-items: center;
   justify-content: center;
 
-  & h3 {
-    font-size: 2.4rem;
+  > h3 {
+    font-size: 2.6rem;
+  }
+`;
+
+// Header
+
+export const StyledHeader = styled.div`
+  height: 4.8rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 3.6rem;
+  padding: 1.6rem 3.6rem;
+  border-bottom: solid 1px #d3d3d3;
+
+  a {
+    font-weight: 500;
+    font-size: 2rem;
+  }
+
+  ul {
+    flex: 1;
+    display: flex;
+    list-style: none;
+    gap: 2.4rem;
+
+    > a {
+      font-size: 1.6rem;
+    }
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 1.8rem;
+
+    > a {
+      font-size: 2.4rem;
+    }
   }
 `;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,33 @@
+import { StyledHeader } from "../styles/Styles";
+import { LiaShoppingBagSolid } from "react-icons/lia";
+import { LuUser } from "react-icons/lu";
+import { IoSearchOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <StyledHeader>
+      <Link to="/home">DigitalPro</Link>
+      <ul>
+        <Link to="/store">Store</Link>
+        <Link to="/">Mobiles</Link>
+        <Link to="/">TV & Display</Link>
+        <Link to="/">Laptop</Link>
+        <Link to="/">Accessories</Link>
+      </ul>
+      <div>
+        <Link to="/">
+          <IoSearchOutline />
+        </Link>
+        <Link to="/cart">
+          <LiaShoppingBagSolid />
+        </Link>
+        <Link to="/">
+          <LuUser />
+        </Link>
+      </div>
+    </StyledHeader>
+  );
 }
 
 export default Header;
