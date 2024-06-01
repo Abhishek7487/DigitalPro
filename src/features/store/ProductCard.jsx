ba72b7536190edd5842673b20208d3686d3b7d1e
Megha-Bathla <bathlamegha6@gmail.com>
2024-06-01 20:07:55 +0530

    refactor product card

--- src/features/store/ProductCard.jsx
+++ src/features/store/ProductCard.jsx
@@ -1,22 +1,47 @@
 import React from "react";
 import { StyledProductCard } from "../../styles/Styles";
 import { ImStarEmpty } from "react-icons/im";
+import { ImStarHalf } from "react-icons/im";
+import { ImStarFull } from "react-icons/im";
+
+export const Star = ({ type }) => {
+  if (type === "FULL") {
+    return <ImStarFull style={{ color: "red" }} />;
+  } else if (type === "HALF") {
+    return <ImStarHalf style={{ color: "red" }} />;
+  }
+
+  return <ImStarEmpty style={{ color: "red" }} />;
+};
 
 export default function ProductCard({ product }) {
+  const maxStars = 5; // Max stars to be displayed
+  const filledStars = Math.floor(product.rating);
+  const hasHalfStar = product.rating % 1 !== 0;
+  const emptyStars = maxStars - filledStars - (hasHalfStar ? 1 : 0);
+
+  const stars = [
+    ...Array(filledStars).fill("FULL"),
+    ...(hasHalfStar ? ["HALF"] : []),
+    ...Array(emptyStars).fill("EMPTY"),
+  ];
+
   return (
     <StyledProductCard>
       <div className="productImage">
-        <img src={product.productImages[0]} />
+        <img src={product.productImages[0]} alt={product.name} />
       </div>
 
       <div className="productInfo">
         <p>{product.name}</p>
         <main>
-          <p>₹{product.price}</p>
           <span>
-            <ImStarEmpty style={{ color: "red" }} />
             {product.rating}
+            {stars.map((type, index) => (
+              <Star key={index} type={type} />
+            ))}
           </span>
+          <p>₹{product.price}</p>
         </main>
         <button>Add to Cart</button>
       </div>
