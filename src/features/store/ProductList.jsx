8c052d0a13ee96d00b29b5d77f9cf307894c13b4
Abhishek7487 <dellabhishek7487@gmail.com>
2024-06-10 17:23:57 +0530

    Created fully dynamic Product List

--- src/features/store/ProductList.jsx
+++ src/features/store/ProductList.jsx
@@ -3,23 +3,36 @@ import useProducts from "../../services/useProducts";
 import { StyledProductList } from "../../styles/Styles";
 import ProductCard from "./ProductCard";
 
-const getFilteredProducts = (categoryFilter, products) => {
-  if (categoryFilter === "all") return products;
-  return products.filter((product) => product.category === categoryFilter);
-};
-
 function ProductList() {
   const [searchParams] = useSearchParams();
   const products = useProducts();
   const filterValue = searchParams.get("category") || "all";
-  const sortedValue = searchParams.get("price") || "all";
-  const rangeValue = searchParams.get("range") || "all";
+  const sortValue = searchParams.get("price") || "all";
+  const brandValue = searchParams.get("brand") || "all";
+  const ratingValue = searchParams.get("rating") || "";
+
+  const getFilteredProducts = (categoryFilter, products) => {
+    if (categoryFilter === "all") return products;
+    return products.filter((product) => product.category === categoryFilter);
+  };
 
   let filteredProducts = getFilteredProducts(filterValue, products);
 
-  if (sortedValue === "h2l") filteredProducts.sort((a, b) => b.price - a.price);
-  else if (sortedValue === "l2h")
+  if (sortValue === "h2l") filteredProducts.sort((a, b) => b.price - a.price);
+  else if (sortValue === "l2h")
     filteredProducts.sort((a, b) => a.price - b.price);
+
+  if (brandValue !== "all") {
+    filteredProducts = filteredProducts.filter(
+      (product) => product.brand === brandValue
+    );
+  }
+
+  if (ratingValue) {
+    filteredProducts = filteredProducts.filter(
+      (product) => product.rating >= ratingValue
+    );
+  }
 
   return (
     <StyledProductList>
