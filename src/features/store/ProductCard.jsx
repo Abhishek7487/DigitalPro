0717dfb5daab92a36f6d44b6aed9f8875cc5fd16
Abhishek7487 <dellabhishek7487@gmail.com>
2024-06-11 16:05:30 +0530

    Create Route for Product Page and fetch product

--- src/features/store/ProductCard.jsx
+++ src/features/store/ProductCard.jsx
@@ -2,25 +2,28 @@ import React from "react";
 import { StyledProductCard } from "../../styles/Styles";
 import Star from "../../services/Star";
 import { formatter } from "../../utils/useCurrencyFormatter";
+import { Link } from "react-router-dom";
 
 export default function ProductCard({ product }) {
   return (
-    <StyledProductCard>
-      <div className="productImage">
-        <img src={product.productImages[0]} alt={product.name} />
-      </div>
+    <Link to={`/store/${product.id}`}>
+      <StyledProductCard>
+        <div className="productCardImage">
+          <img src={product.productImages[0]} alt={product.name} />
+        </div>
 
-      <div className="productInfo">
-        <p>{product.name}</p>
-        <main>
-          <span>
-            {product.rating}
-            <Star rating={product.rating} />
-          </span>
-          <p>{formatter.format(product.price)}</p>
-        </main>
-        <button>Add to Cart</button>
-      </div>
-    </StyledProductCard>
+        <div className="productCardInfo">
+          <p>{product.name}</p>
+          <main>
+            <span>
+              {product.rating}
+              <Star rating={product.rating} />
+            </span>
+            <p>{formatter.format(product.price)}</p>
+          </main>
+          <button>Add to Cart</button>
+        </div>
+      </StyledProductCard>
+    </Link>
   );
 }
